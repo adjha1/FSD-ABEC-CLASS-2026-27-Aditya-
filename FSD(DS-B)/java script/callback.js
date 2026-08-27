38a7da7da491474dea2055d490d6b4ad94e4f63b
Aditya Kumar Jha <aditya.ibm.kumar@gmail.com>
2026-08-27 10:42:10 +0530

    js

--- FSD(DS-B)/java script/callback.js
+++ FSD(DS-B)/java script/callback.js
@@ -1,11 +1,40 @@
-function sum(a, b) {
-    return a + b;
-}
-function sumWithMsg(clbk, msg) {
-    const result = clbk(20, 30);
-    const fresult = "HI" + msg + "Your score is=" + result;
-    console.log(fresult);
+// // function sum(a, b) {
+// //     return a + b;
+// // }
+// // function sumWithMsg(clbk, msg) {
+// //     const result = clbk(20, 30);
+// //     const fresult = "HI" + msg + "Your score is=" + result;
+// //     console.log(fresult);
 
-}
+// // }
 
-sumWithMsg(sum, "MR. Deepak");
+// // sumWithMsg(sum, "MR. Deepak");
+
+
+
+
+
+
+
+
+
+
+// function sum(a,b){
+//     return a+b
+// }
+// function sumwithMsg(clbk,msg){
+//     const result=clbk(10,20);
+//     const fresult="hi"+msg+"sum is"+result;
+//     console.log(fresult);
+// }
+// sumwithMsg(sum,"mr. deepak");
+
+
+
+
+
+function(callback ){
+const result = 2*2
+callback(result)
+}
+console.log(callback(result))
