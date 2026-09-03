ef08df3e07923177a3fcd3ca019dfbd93f14c7e5
Aditya Kumar Jha <aditya.ibm.kumar@gmail.com>
2026-09-03 10:12:04 +0530

    js

---
+++ FSD(DS-B)/java script/callbackhell.js
@@ -0,0 +1,33 @@
+function test1(cb) {
+    setTimeout(() => {
+        console.log("test1")
+        cb();
+    }, 4000);
+}
+function test2(cb) {
+    setTimeout(() => {
+        console.log("test2")
+        cb();
+    }, 2000);
+}
+function test3(cb){
+    setTimeout(()=>{
+        console.log(("task3"))
+        cb();
+    },200);
+}
+function test4(cb){
+    setTimeout(()=>{
+        console.log(("task4"))
+        cb();
+    },200);
+}
+
+test1(()=>{
+    test2(()=>{
+        test3(()=>{
+            test4();
+        });
+    });
+});
+
