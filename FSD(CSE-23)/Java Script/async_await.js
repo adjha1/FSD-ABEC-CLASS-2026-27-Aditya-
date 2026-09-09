532d2b354dad94dda9002b32625ed9e4fb4ba1ff
Aditya Kumar Jha <aditya.ibm.kumar@gmail.com>
2026-09-09 10:29:40 +0530

    js

---
+++ FSD(CSE-23)/Java Script/async_await.js
@@ -0,0 +1,32 @@
+function f1() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("HI!!!")
+            resolve();
+        }, 4000);
+    })
+
+}
+function f2() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("ABES COllege");
+            resolve();
+        }, 2000);
+    })
+
+}
+
+
+
+async function test() {
+    try {
+        await f1();
+        await f2();
+    }
+    catch (err) {
+        console.log("ERROR", err)
+    }
+}
+
+test();
