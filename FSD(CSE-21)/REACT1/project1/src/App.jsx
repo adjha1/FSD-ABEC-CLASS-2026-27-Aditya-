2aa3d84e10975d75fd245dafabc6d0ad61fe239f
Aditya Kumar Jha <aditya.ibm.kumar@gmail.com>
2026-09-09 15:32:53 +0530

    react

---
+++ FSD(CSE-21)/REACT1/project1/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from './components/Card'
+
+const App = () => {
+  return (
+    <div>
+      <Card />
+      <br />
+      <Card />
+      <br />
+      <Card />
+      <br />
+    </div>
+  )
+}
+
+export default App
