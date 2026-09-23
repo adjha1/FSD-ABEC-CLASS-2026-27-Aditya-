87ee646cf0730dd029a790e8dff8a57bf0be8105
Aditya Kumar Jha <aditya.ibm.kumar@gmail.com>
2026-09-23 12:00:19 +0530

    js

---
+++ FSD(CSE-23)/React1/React_Project/react/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Student from './components/Student'
+
+const App = () => {
+  return (
+    <div>
+      <h1>STUDENT RECORD</h1>
+      <div>
+        <Student />
+      </div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ FSD(CSE-23)/React1/React_Project/react/src/components/Student.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Student = () => {
+    return (
+        <div>
+            <div style={{ backgroundColor: 'yellowgreen', border: '2px solid red', height: '300px', width: '300px' }}>
+                <h2>Mohan Ji</h2>
+                <img src="https://img.magnific.com/free-photo/3d-cartoon-character_23-2151021986.jpg" alt="" height={'100px'} width={'100px'} />
+                <h3>R0ll No:101</h3>
+                <h3>CLASS:B.Tech(CSE-23)</h3>
+
+            </div>
+        </div>
+    )
+}
+
+export default Student
