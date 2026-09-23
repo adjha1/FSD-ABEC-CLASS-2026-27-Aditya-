a2c367959cf55fbe44a2567635e575d68cec88b6
Aditya Kumar Jha <aditya.ibm.kumar@gmail.com>
2026-09-23 15:33:24 +0530

    react

--- FSD(CSE-21)/REACT1/project1/src/App.jsx
+++ FSD(CSE-21)/REACT1/project1/src/App.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
-import Card from './components/Card'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
+function Home() {
+  return <h1>THIS IS MY HOME PAGE</h1>
+}
+function About() {
+  return <h1>THIS IS MY ABOUT PAGE</h1>
+
+}
+function Phone() {
+  return <h1>THIS IS MY PHONE NO PAGE</h1>
+}
 const App = () => {
   return (
     <div>
-      <Card />
-      <br />
-      <Card />
-      <br />
-      <Card />
-      <br />
+      <BrowserRouter>
+        <nav>
+          <Link to="/">HOME PAGE</Link> |{" "}
+          <Link to="/about">ABOUT PAGE</Link> |{" "}
+          <Link to="/phone">PHONE NO</Link> |{" "}
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/phone" element={<Phone />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   )
 }
--- FSD(CSE-21)/REACT1/project1/src/components/Card.jsx
+++ FSD(CSE-21)/REACT1/project1/src/components/Card.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div>
             <div style={{ border: '2px solid red', height: '300px', width: '300px' }}>
                 <h1>MY CAR</h1>
                 <p> <img src="https://img.magnific.com/premium-vector/blue-car-emoticon-funny-car-face-character-smiles-icons-vector-illustration_1041562-1753.jpg?semt=ais_hybrid&w=740&q=80" alt="" height={'100px'} width={'100px'} /> </p>
-                <h2>CAR NAME:AULTO 800</h2>
-                <h2>PRICE: 500000</h2>
+                <h2>CAR NAME:{props.name}</h2>
+                <h2>PRICE:{props.pri}</h2>
             </div>
         </div>
     )
