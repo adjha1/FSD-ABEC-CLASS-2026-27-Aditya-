a647018ae1255c7b23fed95c8086dc48ffd16dcb
Aditya Kumar Jha <aditya.ibm.kumar@gmail.com>
2026-09-24 14:32:54 +0530

    react

--- FSD(DS-A)/React/myproject/src/App.jsx
+++ FSD(DS-A)/React/myproject/src/App.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import Card from './components/Card'
-
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+function Home() {
+  return <h1>This is  my Home Page</h1>
+}
+function About() {
+  return <h1>Tis is my about Us page</h1>
+}
 const App = () => {
   return (
-    <div>
 
-      <Card />
-      <br />
-      <br />
-      <Card />
-      <br />
-      <br />
-      <Card />
-      <br />
-      <br />
+    <BrowserRouter>
+      <nav>
+        <Link to="/">HOME</Link>
+        <Link to="/about">ABOUT US</Link>
+      </nav>
 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
 
-    </div>
   )
 }
 
